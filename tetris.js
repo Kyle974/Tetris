57d046ca89a4772b18debc38ddc19444bc0a0fe5
Kyle974 <https://github.com/Kyle974>
2019-11-05 15:39:31 +0000

    creating player value

--- tetris.js
+++ tetris.js
@@ -8,11 +8,20 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
-matrix.forEach((row, y) => {
-  row.forEach((value, x) => {
-    if (value !== 0) {
-      context.fillStyle = "red";
-      context.fillRect(x, y, 1, 1);
-    }
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = "red";
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
   });
-});
+}
+
+const player = {
+  pos: { x: 5, y: 5 },
+  matrix: matrix
+};
+
+drawMatrix(player.matrix, player.pos);
