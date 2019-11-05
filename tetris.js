d1e153a421b6794b2dff3ff93dfe92e09dfd8620
Kyle974 <https://github.com/Kyle974>
2019-11-05 17:46:43 +0000

    implimenting left, right and drop functionality to 'update' function

--- tetris.js
+++ tetris.js
@@ -23,8 +23,20 @@ function drawMatrix(matrix, offset) {
   });
 }
 
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
 function update(time = 0) {
-  console.log(time);
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
+
   draw();
   requestAnimationFrame(update);
 }
@@ -33,5 +45,19 @@ const player = {
   pos: { x: 5, y: 5 },
   matrix: matrix
 };
+
+document.addEventListener("keydown", (event) => {
+  console.log(event.keyCode);
+  if (event.keyCode === 37) {
+    player.pos.x--;
+  }
+  if (event.keyCode === 39) {
+    player.pos.x++;
+  }
+  if (event.keyCode === 40) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
+});
 
 update();
