13ea695bb0a208fc951968d9d9967bb4bf4a0c94
Kyle974 <https://github.com/Kyle974>
2019-11-05 22:10:36 +0000

    implimenting collision functionality

--- tetris.js
+++ tetris.js
@@ -5,10 +5,31 @@ context.scale(20, 20);
 
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(width, height) {
+  const matrix = [];
+  while (height--) {
+    matrix.push(new Array(width).fill(0));
+  }
+  return matrix;
+}
+
 function draw() {
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
+  drawMatrix(arena, { x: 0, y: 0 });
   drawMatrix(player.matrix, player.pos);
 }
 
@@ -21,6 +42,33 @@ function drawMatrix(matrix, offset) {
       }
     });
   });
+}
+
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+function playerDrop() {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+}
+
+function playerMove(dir) {
+  player.pos.x += dir;
+  if (collide(arena, player)) {
+    player.pos.x -= dir;
+  }
 }
 
 let dropCounter = 0;
@@ -41,22 +89,22 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+console.log(arena);
+console.table(arena);
+
 const player = {
   pos: { x: 5, y: 5 },
   matrix: matrix
 };
 
 document.addEventListener("keydown", (event) => {
-  console.log(event.keyCode);
   if (event.keyCode === 37) {
-    player.pos.x--;
-  }
-  if (event.keyCode === 39) {
-    player.pos.x++;
-  }
-  if (event.keyCode === 40) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerMove(-1);
+  } else if (event.keyCode === 39) {
+    playerMove(1);
+  } else if (event.keyCode === 40) {
+    playerDrop();
   }
 });
 
