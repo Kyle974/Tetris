50ae6d861bb803a0c8b3393ecd7cb7301040c3c2
Kyle974 <https://github.com/Kyle974>
2019-11-06 15:58:44 +0000

    adding additional piece shapes

--- tetris.js
+++ tetris.js
@@ -38,6 +38,18 @@ function drawMatrix(matrix, offset) {
 function createPiece(type) {
   if (type === "T") {
     return [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
+  } else if (type === "O") {
+    return [[1, 1], [1, 1]];
+  } else if (type === "L") {
+    return [[0, 1, 0], [0, 1, 0], [0, 1, 1]];
+  } else if (type === "J") {
+    return [[0, 1, 0], [0, 1, 0], [1, 1, 0]];
+  } else if (type === "I") {
+    return [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]];
+  } else if (type === "S") {
+    return [[0, 1, 1], [1, 1, 0], [0, 0, 0]];
+  } else if (type === "Z") {
+    return [[1, 1, 0], [0, 1, 1], [0, 0, 0]];
   }
 }
 
@@ -128,7 +140,7 @@ console.table(arena);
 
 const player = {
   pos: { x: 5, y: 5 },
-  matrix: createPiece("T")
+  matrix: createPiece("Z")
 };
 
 document.addEventListener("keydown", (event) => {
