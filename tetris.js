6fc1042664fda1e76d923b3eff4f6062f12f459e
Kyle974 <https://github.com/Kyle974>
2019-11-06 16:35:15 +0000

    adding different colours to pieces

--- tetris.js
+++ tetris.js
@@ -28,7 +28,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = "red";
+        context.fillStyle = colours[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
@@ -39,17 +39,17 @@ function createPiece(type) {
   if (type === "T") {
     return [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
   } else if (type === "O") {
-    return [[1, 1], [1, 1]];
+    return [[2, 2], [2, 2]];
   } else if (type === "L") {
-    return [[0, 1, 0], [0, 1, 0], [0, 1, 1]];
+    return [[0, 3, 0], [0, 3, 0], [0, 3, 3]];
   } else if (type === "J") {
-    return [[0, 1, 0], [0, 1, 0], [1, 1, 0]];
+    return [[0, 4, 0], [0, 4, 0], [4, 4, 0]];
   } else if (type === "I") {
-    return [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]];
+    return [[0, 5, 0, 0], [0, 5, 0, 0], [0, 5, 0, 0], [0, 5, 0, 0]];
   } else if (type === "S") {
-    return [[0, 1, 1], [1, 1, 0], [0, 0, 0]];
+    return [[0, 6, 6], [6, 6, 0], [0, 0, 0]];
   } else if (type === "Z") {
-    return [[1, 1, 0], [0, 1, 1], [0, 0, 0]];
+    return [[7, 7, 0], [0, 7, 7], [0, 0, 0]];
   }
 }
 
@@ -144,6 +144,17 @@ function update(time = 0) {
   draw();
   requestAnimationFrame(update);
 }
+
+const colours = [
+  null,
+  "red",
+  "blue",
+  "violet",
+  "green",
+  "purple",
+  "orange",
+  "pink,"
+];
 
 const arena = createMatrix(12, 20);
 console.log(arena);
